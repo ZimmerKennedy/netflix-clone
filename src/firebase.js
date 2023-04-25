220db3ad170ce73d0425e0ae9303bc4b559f5447
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-04-24 20:41:13 -0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2CwWxRgTuJ9mAd14H_VvEG_Tf_1_A2-8",
+    authDomain: "web-app-clone-netflix.firebaseapp.com",
+    projectId: "web-app-clone-netflix",
+    storageBucket: "web-app-clone-netflix.appspot.com",
+    messagingSenderId: "732524391441",
+    appId: "1:732524391441:web:54bd8c299d6f75531c15b0"
+  };
