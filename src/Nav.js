9bc7ca57fa6c99103b4bf3a108b45db40a402574
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-04-27 13:13:11 -0800

    added-arrows

--- src/Nav.js
+++ src/Nav.js
@@ -21,7 +21,7 @@ const Nav = () => {
       <div className="nav__contents">
         <img
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="/netflixLogo.png"
           alt=""
           onClick={() => navigate("/home")}
         />
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Row.css";
 import axios from "./axios";
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
 
+  const scrollLeft = () => {
+    if (rowRef.current) {
+      rowRef.current.scrollLeft -= window.innerWidth - 200;
+    }
+  };
+
+  const scrollRight = () => {
+    if (rowRef.current) {
+      rowRef.current.scrollLeft += window.innerWidth - 200;
+    }
+  };
+
+  const rowRef = useRef(null);
+
   const base_url = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(fetchUrl);
-      console.log(`request`,request)
+      console.log(`request`, request);
       setMovies(request.data.results);
       return request;
     };
@@ -19,7 +33,10 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row__posters">
+      <div className="row__posters" ref={rowRef}>
+      <button className="row__scrollLeft" onClick={scrollLeft}>
+        &lt;
+      </button>
         {movies.map((movie) => {
           const shouldRenderImage =
             (isLargeRow && movie.poster_path) ||
@@ -30,14 +47,15 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               key={movie.id}
               src={`${base_url}${
-                isLargeRow ?  
-                movie.backdrop_path : movie.poster_path 
-
+                isLargeRow ? movie.backdrop_path : movie.poster_path
               }`}
               alt={movie.name}
             />
           ) : null;
         })}
+      <button className="row__scrollRight" onClick={scrollRight}>
+        &gt;
+      </button>
       </div>
     </div>
   );
